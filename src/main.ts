1e843160a264e3408d14c21043c15715f88c2aef
jobs <jobs@space.local>
2026-02-12 22:57:31 +1100

    fix(api): update URL to space-api.fly.dev
    space: d/b042d1b0

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'https://spacebrr-api.fly.dev'
+const API_URL = import.meta.env.VITE_API_URL || 'https://space-api.fly.dev'
 
 const app = document.getElementById('app')!
 
